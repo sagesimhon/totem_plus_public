d675db31d038f398e73e691d8db4dbc9e0a5f7d7
Sage <38890551+sagesimhon@users.noreply.github.com>
2024-12-25 12:45:42 -0500

    Save work in progress on main

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ mi_variant = 'llvm_ad_rgb'
 # Local:
 input_data_base_path = '/Users/sage/PycharmProjects/totem_new_shapes/data'
 # output_data_base_path = '/Users/sage/experiments'
-output_data_base_path = '/Users/sage/realworldtotems'
+# output_data_base_path = '/Users/sage/realworldtotems'
+output_data_base_path = '/Users/sage/OldLaptop/sage/data-for-totems-new-shapes'
 
 # Remote:
 # input_data_base_path = '/afs/csail.mit.edu/u/s/simhon/totem_plus/data'
